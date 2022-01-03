ac511e668f1461b2fdd68c71ca3df9b7faf9901f
FlorianWoelki <FlorianWoelki@gmx.de>
2022-01-03 11:58:17 +0100

    feat: add electron support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
-import { defineConfig } from 'vite';
+import { UserConfig, ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
+import { join } from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), monacoEditorPlugin()],
-});
+const srcRoot = join(__dirname, 'src');
+
+export default ({ command }: ConfigEnv): UserConfig => {
+  if (command === 'serve') {
+    return {
+      root: srcRoot,
+      base: '/',
+      plugins: [react(), monacoEditorPlugin()],
+      build: {
+        outDir: join(srcRoot, '/out'),
+        emptyOutDir: true,
+        rollupOptions: {},
+      },
+      server: {
+        port: process.env.PORT === undefined ? 3000 : +process.env.PORT,
+      },
+      optimizeDeps: {
+        exclude: ['path'],
+      },
+    };
+  }
+
+  return {
+    root: srcRoot,
+    base: `${__dirname}/src/out/`,
+    plugins: [react(), monacoEditorPlugin()],
+    build: {
+      outDir: join(srcRoot, '/out'),
+      emptyOutDir: true,
+      rollupOptions: {},
+    },
+    server: {
+      port: process.env.PORT === undefined ? 3000 : +process.env.PORT,
+    },
+    optimizeDeps: {
+      exclude: ['path'],
+    },
+  };
+};
