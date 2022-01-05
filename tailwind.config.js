7f6a28f74e61bece1b9a17f2372b191d7258d956
FlorianWoelki <FlorianWoelki@gmx.de>
2022-01-05 11:13:55 +0100

    refactor: tailwindcss color extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    colors: {
-      ...colors,
-      iron: {
-        50: '#f4f5f5',
-        100: '#eaeaeb',
-        200: '#cacbcd',
-        300: '#aaacaf',
-        400: '#6b6e72',
-        500: '#2b3036',
-        600: '#272b31',
-        700: '#202429',
-        800: '#1a1d20',
-        900: '#15181a',
+    extend: {
+      colors: {
+        iron: {
+          50: '#f4f5f5',
+          100: '#eaeaeb',
+          200: '#cacbcd',
+          300: '#aaacaf',
+          400: '#6b6e72',
+          500: '#2b3036',
+          600: '#272b31',
+          700: '#202429',
+          800: '#1a1d20',
+          900: '#15181a',
+        },
       },
     },
-    extend: {},
   },
   plugins: [],
 };
