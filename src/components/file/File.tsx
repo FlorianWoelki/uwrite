8af37dce25f166ac7c594a1629388d0fcebce71d
FlorianWoelki <FlorianWoelki@gmx.de>
2022-03-07 12:32:29 +0100

    feat: add pencil and trash icon

--- src/components/file/File.tsx
+++ src/components/file/File.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import { ReactComponent as PencilIcon } from '../../../assets/icons/pencil.svg';
+import { ReactComponent as TrashIcon } from '../../../assets/icons/trash.svg';
+
 interface FileProps {
   active?: boolean;
   children: React.ReactNode;
@@ -9,13 +13,21 @@ export const File: React.FC<FileProps> = ({
 }): JSX.Element => {
   return (
     <li
-      className={`cursor-pointer rounded px-3 py-1 transition duration-150 ease-in-out hover:text-iron-900 ${
+      className={`flex cursor-pointer justify-between rounded px-3 py-1 transition duration-150 ease-in-out hover:text-iron-900 ${
         active
           ? 'bg-iron-200 text-iron-900 dark:bg-iron-500 dark:text-iron-200'
           : 'text-iron-400'
       }`}
     >
-      {children}
+      <span>{children}</span>
+      <div className="flex items-center space-x-1 text-iron-300">
+        <button className="transition duration-150 ease-in-out hover:text-iron-400">
+          <PencilIcon className="h-4 w-4" />
+        </button>
+        <button className="transition duration-150 ease-in-out hover:text-iron-400">
+          <TrashIcon className="h-4 w-4" />
+        </button>
+      </div>
     </li>
   );
 };
