a0e89be19c5cd01917f7b59af186d415bd2eb7f7
FlorianWoelki <FlorianWoelki@gmx.de>
2022-06-07 20:00:04 +0200

    fix(hooks): issue with empty localStorage value

--- src/hooks/useVim.ts
+++ src/hooks/useVim.ts
@@ -3,12 +3,14 @@ import { useEffect, useState } from 'react';
 const localStorageKey = 'isVimActive';
 
 export const useVim = (): [boolean, (isActive: boolean) => void] => {
-  const [isVimActive, setIsVimActive] = useState<boolean>(true);
+  const [isVimActive, setIsVimActive] = useState<boolean>(false);
 
   useEffect(() => {
-    const isActive =
-      JSON.parse(localStorage.getItem(localStorageKey) ?? '') ?? true;
-    setIsVimActive(isActive);
+    const item = localStorage.getItem(localStorageKey);
+    if (item) {
+      const isActive = JSON.parse(item) ?? false;
+      setIsVimActive(isActive);
+    }
   }, []);
 
   const toggleVim = (isActive: boolean): void => {
