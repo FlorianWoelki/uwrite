f5e63c3898f548cbce7c188eb393117d08a06900
FlorianWoelki <FlorianWoelki@gmx.de>
2023-04-07 10:57:24 +0200

    build: optimize vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { UserConfig, ConfigEnv } from 'vite';
+import { UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -6,24 +6,7 @@ import { join } from 'path';
 
 const srcRoot = join(__dirname, 'src');
 
-export default ({ command }: ConfigEnv): UserConfig => {
-  if (command === 'serve') {
-    return {
-      root: srcRoot,
-      plugins: [
-        react(),
-        monacoEditorPlugin({}),
-        svgrPlugin({ svgrOptions: { icon: true } }),
-      ],
-      server: {
-        port: process.env.PORT === undefined ? 3000 : +process.env.PORT,
-      },
-      optimizeDeps: {
-        exclude: ['path'],
-      },
-    };
-  }
-
+export default (): UserConfig => {
   return {
     root: srcRoot,
     plugins: [
