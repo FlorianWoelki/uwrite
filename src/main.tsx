cc22222a770ce99fea4f97cb5472cad8b820e28a
FlorianWoelki <FlorianWoelki@gmx.de>
2023-04-08 10:42:10 +0200

    refactor: switch to new react root render

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+
+if (container) {
+  const root = ReactDOMClient.createRoot(container);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+}
