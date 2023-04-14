4a9971001699e9d02b615da001224058fdc2d864
FlorianWoelki <FlorianWoelki@gmx.de>
2023-04-14 09:09:57 +0200

    docs(util): add function comment to debounce function

--- src/util/effects.ts
+++ src/util/effects.ts
@@ -1,3 +1,9 @@
+/**
+ * Debounces a callback and waits a specific amount of time before executing the callback.
+ * @param callback Function that is being called when the timeout is done.
+ * @param timeout Optional timeout that will be used for the timer (default is `400`ms).
+ * @returns Function that can be called to clear the timeout and start a new timeout.
+ */
 export const debounce = (callback: () => void, timeout: number = 400) => {
   let timer: NodeJS.Timeout;
   return (): void => {
