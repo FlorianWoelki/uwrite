d0fb4b98e91b9eb4aadd78d04aa1abe68b00c59e
FlorianWoelki <FlorianWoelki@gmx.de>
2023-04-19 08:59:21 +0200

    refactor(comp): change button classes and button props

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,39 +1,34 @@
 import clsx from 'clsx';
 
 interface ButtonProps {
+  /**
+   * Whether or not the button is in a button group.
+   * @defaultValue `false`
+   */
   grouped?: boolean;
-  className?: string;
-  children: React.ReactNode;
+  /**
+   * Whether or not the button is in an active state or not.
+   * @defaultValue `false`
+   */
   active?: boolean;
-  disabled?: boolean;
-  onClick?: () => void;
+  className?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-  grouped,
-  children,
-  className,
-  active,
-  ...rest
-}): JSX.Element => {
-  const extraClasses = clsx(
-    {
-      'border rounded': !grouped,
-    },
-    className,
-  );
-
+export const Button: React.FC<
+  ButtonProps & JSX.IntrinsicElements['button']
+> = ({ grouped, className, active, ...rest }): JSX.Element => {
   return (
     <button
       type="button"
-      className={`border-iron-200 px-4 py-2 text-iron-500 transition duration-150 ease-in-out hover:bg-iron-200 hover:text-iron-600 disabled:opacity-50 dark:border-iron-900 dark:text-iron-200 dark:hover:bg-iron-600 dark:hover:text-iron-200 ${extraClasses} ${
+      className={clsx(
+        'border-iron-200 px-4 py-2 text-iron-500 transition duration-150 ease-in-out hover:bg-iron-200 hover:text-iron-600 disabled:opacity-50 dark:border-iron-900 dark:text-iron-200 dark:hover:bg-iron-600 dark:hover:text-iron-200',
         active
           ? 'bg-iron-200 dark:bg-iron-600 dark:text-iron-200'
-          : 'dark:bg-iron-700 bg-iron-100'
-      }`}
+          : 'dark:bg-iron-700 bg-iron-100',
+        !grouped ? 'border rounded' : undefined,
+        className,
+      )}
       {...rest}
-    >
-      {children}
-    </button>
+    ></button>
   );
 };
