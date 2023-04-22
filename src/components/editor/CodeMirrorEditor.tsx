a7aafef95e5ff28a6e3e6413b6e20bb90c03ab64
FlorianWoelki <FlorianWoelki@gmx.de>
2023-04-22 10:32:01 +0200

    refactor(comp): move math and katex extension to custom file

--- src/components/editor/CodeMirrorEditor.tsx
+++ src/components/editor/CodeMirrorEditor.tsx
@@ -1,36 +1,8 @@
 import { useEffect } from 'react';
 import { markdown } from '@codemirror/lang-markdown';
-import { tags as t } from '@lezer/highlight';
-import {
-  BlockContext,
-  LeafBlock,
-  LeafBlockParser,
-  Line,
-} from '@lezer/markdown';
 import { useCodeEditor } from '../../hooks/useCodeEditor';
 import './CodeMirrorEditor.css';
-
-const katexDelim = {
-  resolve: 'Katex',
-  mark: 'KatexMark',
-};
-
-class MathBlockParser implements LeafBlockParser {
-  nextLine(): boolean {
-    return false;
-  }
-
-  finish(cx: BlockContext, leaf: LeafBlock): boolean {
-    cx.addLeafElement(
-      leaf,
-      cx.elt('KatexBlock', leaf.start, leaf.start + leaf.content.length, [
-        cx.elt('KatexBlockMark', leaf.start, leaf.start + 3),
-        ...cx.parser.parseInline(leaf.content.slice(3), leaf.start + 3),
-      ]),
-    );
-    return true;
-  }
-}
+import { mathExtensions } from './extensions';
 
 export const CodeMirrorEditor: React.FC<any> = ({
   value,
@@ -47,72 +19,10 @@ export const CodeMirrorEditor: React.FC<any> = ({
     extensions: [
       ...extensions,
       markdown({
-        extensions: [
-          // Custom katex highlighting.
-          {
-            defineNodes: [
-              {
-                name: 'Katex',
-                style: { 'Katex/...': t.atom },
-              },
-              {
-                name: 'KatexMark',
-                style: t.processingInstruction,
-              },
-            ],
-            parseInline: [
-              {
-                name: 'Katex',
-                parse: (cx, next, pos) => {
-                  const character: number = 36; // $
-                  if (next !== character) {
-                    return -1;
-                  }
-
-                  return cx.addDelimiter(katexDelim, pos, pos + 1, true, true);
-                },
-              },
-            ],
-          },
-          {
-            defineNodes: [
-              {
-                name: 'KatexBlock',
-                style: { 'KatexBlock/...': t.atom },
-              },
-              {
-                name: 'KatexBlockMark',
-                style: t.processingInstruction,
-              },
-            ],
-            parseBlock: [
-              {
-                name: 'KatexBlock',
-                leaf: (_, leaf) => {
-                  return /^\$\$/.test(leaf.content)
-                    ? new MathBlockParser()
-                    : null;
-                },
-              },
-            ],
-          },
-        ],
+        extensions: mathExtensions,
       }),
     ],
   });
-
-  useEffect(() => {
-    if (view) {
-      // TODO: Is there a more effective way to dispatch this effect?
-      // view.dispatch({
-      //   effects: StateEffect.reconfigure.of([
-      //     ...defaultExtensions,
-      //     ...extensions,
-      //     markdown(),
-      //   ]),
-      // });
-    }
-  }, [extensions]);
 
   useEffect(() => {
     if (view) {
---
+++ src/components/editor/extensions.ts
@@ -0,0 +1,78 @@
+import {
+  BlockContext,
+  LeafBlock,
+  LeafBlockParser,
+  MarkdownExtension,
+} from '@lezer/markdown';
+import { tags as t } from '@lezer/highlight';
+
+const katexDelim = {
+  resolve: 'Katex',
+  mark: 'KatexMark',
+};
+
+class MathBlockParser implements LeafBlockParser {
+  nextLine(): boolean {
+    return false;
+  }
+
+  finish(cx: BlockContext, leaf: LeafBlock): boolean {
+    cx.addLeafElement(
+      leaf,
+      cx.elt('KatexBlock', leaf.start, leaf.start + leaf.content.length, [
+        cx.elt('KatexBlockMark', leaf.start, leaf.start + 3),
+        ...cx.parser.parseInline(leaf.content.slice(3), leaf.start + 3),
+      ]),
+    );
+    return true;
+  }
+}
+
+export const mathExtensions: MarkdownExtension[] = [
+  // Custom katex highlighting.
+  {
+    defineNodes: [
+      {
+        name: 'Katex',
+        style: { 'Katex/...': t.atom },
+      },
+      {
+        name: 'KatexMark',
+        style: t.processingInstruction,
+      },
+    ],
+    parseInline: [
+      {
+        name: 'Katex',
+        parse: (cx, next, pos) => {
+          const character: number = 36; // $
+          if (next !== character) {
+            return -1;
+          }
+
+          return cx.addDelimiter(katexDelim, pos, pos + 1, true, true);
+        },
+      },
+    ],
+  },
+  {
+    defineNodes: [
+      {
+        name: 'KatexBlock',
+        style: { 'KatexBlock/...': t.atom },
+      },
+      {
+        name: 'KatexBlockMark',
+        style: t.processingInstruction,
+      },
+    ],
+    parseBlock: [
+      {
+        name: 'KatexBlock',
+        leaf: (_, leaf) => {
+          return /^\$\$/.test(leaf.content) ? new MathBlockParser() : null;
+        },
+      },
+    ],
+  },
+];
